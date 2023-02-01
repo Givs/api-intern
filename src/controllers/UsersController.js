1a929681990de68b04227b4add365a5bc05a3a90
Givaldo Neto <valdingivs@outlook.com>
2023-02-01 16:33:43 -0300

    refactor: clean controller to use via dependencies injections

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,27 +1,15 @@
-const AppError = require("../utils/AppError");
-const { hash } = require("bcryptjs");
-
-const knex = require('knex');
-const config = require('../../knexfile');
-const db = knex(config['development']);
+const CreateUsersRepository = require("../repositories/createUsersRepository");
+const UserCreateService = require("../services/UserCreateService");
 
 class UsersController {
     async create(req, res) {
-        const { name, email, password, isAdmin } = req.body
+        const { name, email, password, isAdmin } = req.body;
 
-        const emailExists = await db('users').where({ email }).first();
-        if (emailExists) {
-            throw new AppError('Email already exists', 400);
-        }
+        const createUsersRepository = new CreateUsersRepository();
+        const userCreateService = new UserCreateService(createUsersRepository);
 
-        const hashedPassword = await hash(password, 8);
-
-        try {
-            await db('users').insert({ name, email, password: hashedPassword, isAdmin });
-            return res.status(200).send({ message: 'User created successfully' });
-        } catch (error) {
-            return res.status(500).send({ message: error.message });
-        }
+        await userCreateService.execute({ name, email, password, isAdmin })
+        return res.status(201).json();
 
     }
 }
